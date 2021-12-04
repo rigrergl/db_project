8c367cd85fb6b11147328eaeb5ff07e47d017a0b
rigrergl <rigrergl@gmail>
2021-12-04 07:46:53 -0600

    added files in current state (did some work prior to turning this into a git repository)

---
+++ CreateTablesScript.sql
@@ -0,0 +1,185 @@
+CREATE TABLE DEPARTMENT(
+    dept_id INTEGER NOT NULL,
+    dept_name VARCHAR2(30), 
+    PRIMARY KEY (dept_id)
+);
+
+CREATE TABLE PERSON(
+    person_id INTEGER NOT NULL,
+    f_name VARCHAR2(10),
+    l_name VARCHAR2(20),
+    age INTEGER,
+    gender VARCHAR2(1),
+    addr_line_1 VARCHAR2(50),
+    addr_line_2 VARCHAR(50),
+    city VARCHAR(15),
+    state_code VARCHAR(2),
+    zipcode VARCHAR(16),
+    PRIMARY KEY (person_id)
+);
+
+CREATE TABLE PHONE_NUMBER(
+    person_id INTEGER NOT NULL,
+    phone_number VARCHAR(12) NOT NULL, --number are saved in the following format: 888-888-8888
+    PRIMARY KEY (person_id, phone_number),
+    FOREIGN KEY (person_id) REFERENCES PERSON(person_id)
+);
+
+CREATE TABLE EMPLOYEE(
+    employee_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    emp_rank VARCHAR(15),
+    title VARCHAR(30),
+    supervisor_id INTEGER,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (person_id) REFERENCES PERSON(person_id),
+    FOREIGN KEY (supervisor_id) REFERENCES EMPLOYEE(employee_id)
+);
+
+CREATE TABLE CUSTOMER(
+    customer_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    fav_salesman INTEGER NOT NULL,
+    PRIMARY KEY (customer_id),
+    FOREIGN KEY (person_id) REFERENCES PERSON(person_id),
+    FOREIGN KEY (fav_salesman) REFERENCES EMPLOYEE(employee_id)
+);
+
+CREATE TABLE POTENTIAL_EMPLOYEE(
+    pot_employee_id INTEGER NOT NULL,
+    person_id INTEGER NOT NULL,
+    PRIMARY KEY (pot_employee_id),
+    FOREIGN KEY (person_id) REFERENCES PERSON(person_id)
+);
+
+CREATE TABLE SHIFT(
+    employee_id INTEGER NOT NULL,
+    dept_id INTEGER NOT NULL,
+    start_time DATE NOT NULL,
+    end_time DATE,
+    FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id),
+    FOREIGN KEY (dept_id) REFERENCES DEPARTMENT(dept_id)
+);
+
+CREATE TABLE JOB_POSITION(
+    job_id INTEGER NOT NULL,
+    posted_date DATE,
+    description VARCHAR2(100),
+    dept_id INTEGER NOT NULL,
+    PRIMARY KEY (job_id),
+    FOREIGN KEY (dept_id) REFERENCES DEPARTMENT(dept_id)
+);
+
+CREATE TABLE JOB_APPLICATION(
+    app_id INTEGER NOT NULL,
+    job_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    hired CHAR(1) CHECK (hired IN ('Y', 'N')),
+    PRIMARY KEY (app_id),
+    FOREIGN KEY (job_id) REFERENCES JOB_POSITION(job_id),
+    FOREIGN KEY (candidate_id) REFERENCES PERSON(person_id) 
+);
+
+CREATE TABLE INTERVIEW(
+    app_id INTEGER NOT NULL,
+    interviewer_id INTEGER NOT NULL,
+    interview_time TIMESTAMP WITH TIME ZONE,
+    grade INTEGER CHECK (grade BETWEEN 0 AND 100),
+    FOREIGN KEY (app_id) REFERENCES JOB_APPLICATION(app_id),
+    FOREIGN KEY (interviewer_id) REFERENCES EMPLOYEE(employee_id)
+);
+
+CREATE TABLE PRODUCT_TYPE(
+    type_id INTEGER NOT NULL,
+    list_price NUMBER NOT NULL,
+    PRIMARY KEY (type_id)
+);
+
+CREATE TABLE PRODUCT(
+    prod_id INTEGER NOT NULL,
+    weight INTEGER,
+    prod_type INTEGER NOT NULL,
+    prod_size NUMBER,
+    prod_style VARCHAR2(30),
+    PRIMARY KEY (prod_id),
+    FOREIGN KEY (prod_type) REFERENCES PRODUCT_TYPE(type_id)
+);
+
+CREATE TABLE VENDOR(
+    vendor_id INTEGER NOT NULL,
+    credit_rating NUMBER,
+    purchasing_web_service_url VARCHAR(2000),
+    vendor_name VARCHAR2(50),
+    account_num VARCHAR2(30),
+    addr_line_1 VARCHAR2(50),
+    addr_line_2 VARCHAR(50),
+    city VARCHAR(15),
+    state_code VARCHAR(2),
+    zipcode VARCHAR(16),
+    PRIMARY KEY (vendor_id)
+);
+
+CREATE TABLE PART_TYPE (
+    type_id INTEGER NOT NULL,
+    PRIMARY KEY (type_id)
+);
+
+CREATE TABLE PRODUCT_PART (
+    prod_id INTEGER NOT NULL,
+    part_type INTEGER NOT NULL,
+    current_vendor INTEGER NOT NULL,
+    amount INTEGER, 
+    PRIMARY KEY (prod_id, part_type),
+    FOREIGN KEY (prod_id) REFERENCES PRODUCT(prod_id),
+    FOREIGN KEY (part_type) REFERENCES PART_TYPE(type_id),
+    FOREIGN KEY (current_vendor) REFERENCES VENDOR(vendor_id) 
+);
+
+CREATE TABLE PART_TYPE_LISTING(
+    vendor_id INTEGER NOT NULL,
+    part_type INTEGER NOT NULL,
+    price NUMBER,
+    PRIMARY KEY (vendor_id, part_type),
+    FOREIGN KEY (vendor_id) REFERENCES VENDOR(vendor_id),
+    FOREIGN KEY (part_tYpe) REFERENCES PART_TYPE(type_id) 
+);
+
+CREATE TABLE COMPANY_SITE(
+    site_id INTEGER NOT NULL,
+    site_name VARCHAR2(30),
+    addr_line_1 VARCHAR2(50),
+    addr_line_2 VARCHAR(50),
+    city VARCHAR(15),
+    state_code VARCHAR(2),
+    zipcode VARCHAR(16),
+    PRIMARY KEY (site_id)
+);
+
+CREATE TABLE EMPLOYEE_SITE(
+    employee_id INTEGER NOT NULL,
+    site_id INTEGER NOT NULL,
+    PRIMARY KEY (employee_id, site_id),
+    FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id),
+    FOREIGN KEY (site_id) REFERENCES COMPANY_SITE(site_id)
+);
+
+CREATE TABLE SALE_HISTORY(
+    salesman_id INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
+    prod_id INTEGER NOT NULL,
+    sale_time TIMESTAMP WITH TIME ZONE,
+    site_id INTEGER NOT NULL,
+    FOREIGN KEY (salesman_id) REFERENCES EMPLOYEE(employee_id),
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMER(customer_id),
+    FOREIGN KEY (prod_id) REFERENCES PRODUCT(prod_id),
+    FOREIGN KEY (site_id) REFERENCES COMPANY_SITE(site_id)
+);
+
+CREATE TABLE MONTHLY_SALARY(
+    transaction_num INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
+    pay_date DATE NOT NULL,
+    amount NUMBER NOT NULL,
+    PRIMARY KEY (transaction_num, employee_id),
+    FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id)
+);
---
+++ TestDataGeneration.sql
@@ -0,0 +1,550 @@
+--TRUNCATE ALL TABLES
+TRUNCATE TABLE MONTHLY_SALARY;
+TRUNCATE TABLE SALE_HISTORY;
+TRUNCATE TABLE EMPLOYEE_SITE;
+TRUNCATE TABLE COMPANY_SITE;
+TRUNCATE TABLE PART_TYPE_LISTING;
+TRUNCATE TABLE PRODUCT_PART;
+TRUNCATE TABLE PART_TYPE;
+TRUNCATE TABLE VENDOR;
+TRUNCATE TABLE PRODUCT;
+TRUNCATE TABLE PRODUCT_TYPE;
+TRUNCATE TABLE INTERVIEW;
+TRUNCATE TABLE JOB_APPLICATION;
+TRUNCATE TABLE JOB_POSITION;
+TRUNCATE TABLE SHIFT;
+TRUNCATE TABLE POTENTIAL_EMPLOYEE;
+TRUNCATE TABLE CUSTOMER;
+TRUNCATE TABLE EMPLOYEE;
+TRUNCATE TABLE PHONE_NUMBER;
+TRUNCATE TABLE PERSON;
+TRUNCATE TABLE DEPARTMENT;
+
+--DEPARTMENT
+INSERT INTO DEPARTMENT 
+VALUES (
+    1,
+    'First Department'
+);
+
+--PERSON
+INSERT INTO PERSON 
+VALUES(
+    1,
+    'Rigre', 
+    'Garciandia',
+    19,
+    'M',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+INSERT INTO PERSON 
+VALUES(
+    2,
+    'Mike', 
+    'Robinson',
+    20,
+    'M',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+INSERT INTO PERSON 
+VALUES(
+    3,
+    'Gabriella', 
+    'Spencer',
+    21,
+    'F',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+INSERT INTO PERSON 
+VALUES(
+    4,
+    'Ethan', 
+    'Miller',
+    21,
+    'M',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+INSERT INTO PERSON 
+VALUES(
+    5,
+    'Potential', 
+    'Employee',
+    44,
+    'M',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+INSERT INTO PERSON 
+VALUES(
+    6,
+    'Customer_F', 
+    'Customer_L',
+    44,
+    'M',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+--EMLPOYEE
+
+INSERT INTO EMPLOYEE
+VALUES (
+    1,
+    1,
+    'Senior',
+    'Software Developer',
+    NULL
+);
+
+INSERT INTO EMPLOYEE
+VALUES (
+    2,
+    2,
+    'Junior',
+    'Software Developer',
+    1
+);
+
+INSERT INTO EMPLOYEE
+VALUES (
+    3,
+    3,
+    'Senior',
+    'Graphic Designer',
+    1
+);
+
+INSERT INTO EMPLOYEE
+VALUES (
+    4,
+    4,
+    'Senior',
+    'Salesman',
+    NULL
+);
+
+--POTENTIAL_EMPLOYEE
+INSERT INTO POTENTIAL_EMPLOYEE
+VALUES (
+    1,
+    5
+);
+
+--JOB_POSITION
+INSERT INTO JOB_POSITION
+VALUES (
+    1,
+    '29-OCT-2021',
+    'First job position', 
+    1
+);
+
+INSERT INTO JOB_POSITION
+VALUES (
+    2,
+    '25-NOV-2021',
+    'Second job position', 
+    1
+);
+
+--JOB_APPLICATION
+INSERT INTO JOB_APPLICATION
+VALUES (
+    1,
+    1,
+    1,
+    NULL
+);
+
+INSERT INTO JOB_APPLICATION
+VALUES (
+    2,
+    2,
+    1,
+    NULL
+);
+
+INSERT INTO JOB_APPLICATION
+VALUES (
+    3,
+    1,
+    2,
+    NULL
+);
+
+INSERT INTO JOB_APPLICATION
+VALUES (
+    4,
+    2,
+    2,
+    NULL
+);
+
+--INTERVIEW
+INSERT INTO INTERVIEW
+VALUES (
+    1,
+    2,
+    TIMESTAMP '2021-11-15 8:00:00 -8:00',
+    90
+);
+
+INSERT INTO INTERVIEW
+VALUES (
+    1,
+    2,
+    TIMESTAMP '2021-11-20 8:00:00 -8:00',
+    95
+);
+
+INSERT INTO INTERVIEW
+VALUES (
+    2,
+    1,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    NULL
+);
+
+INSERT INTO INTERVIEW
+VALUES (
+    2,
+    1,
+    TIMESTAMP '2022-2-15 8:00:00 -8:00',
+    NULL
+);
+
+INSERT INTO INTERVIEW
+VALUES (
+    3,
+    1,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    60
+);
+
+INSERT INTO INTERVIEW
+VALUES (
+    4,
+    1,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    70
+);
+
+--MONTHLY_SALARY
+INSERT INTO MONTHLY_SALARY
+VALUES (
+    1,
+    1,
+    '31-OCT-2021',
+    5000.0
+);
+
+INSERT INTO MONTHLY_SALARY
+VALUES (
+    2,
+    1,
+    '29-NOV-2021',
+    5100.0
+);
+
+INSERT INTO MONTHLY_SALARY
+VALUES (
+    3,
+    1,
+    '31-DEC-2021',
+    5500.0
+);
+
+INSERT INTO MONTHLY_SALARY
+VALUES (
+    1,
+    2,
+    '31-OCT-2021',
+    1000.0
+);
+
+INSERT INTO MONTHLY_SALARY
+VALUES (
+    2,
+    2,
+    '29-NOV-2021',
+    2000.0
+);
+
+INSERT INTO MONTHLY_SALARY
+VALUES (
+    3,
+    2,
+    '31-DEC-2021',
+    3000.0
+);
+
+--PRODUCT_TYPE
+INSERT INTO PRODUCT_TYPE 
+VALUES (
+    1,
+    200.0
+);
+INSERT INTO PRODUCT_TYPE 
+VALUES (
+    2,
+    50.0
+);
+
+--PRODUCT
+INSERT INTO PRODUCT
+VALUES (
+    1,
+    20.0,
+    1,
+    20.0,
+    'blue'
+);
+
+INSERT INTO PRODUCT
+VALUES (
+    2,
+    20.0,
+    1,
+    20.0,
+    'red'
+);
+
+INSERT INTO PRODUCT
+VALUES (
+    3,
+    20.0,
+    2,
+    20.0,
+    'green'
+);
+
+INSERT INTO PRODUCT
+VALUES (
+    4,
+    20.0,
+    2,
+    20.0,
+    'yellow'
+);
+
+--CUSTOMER
+INSERT INTO CUSTOMER
+VALUES (
+    1,
+    6,
+    4
+);
+
+--COMPANY_SITE
+INSERT INTO COMPANY_SITE
+VALUES (
+    1,
+    'Test Site 1',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+--SALE_HISTORY
+INSERT INTO SALE_HISTORY
+VALUES (
+    4,
+    1,
+    1,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    1
+);
+
+INSERT INTO SALE_HISTORY
+VALUES (
+    4,
+    1,
+    1,
+    TIMESTAMP '2022-1-16 8:00:00 -8:00',
+    1
+);
+
+INSERT INTO SALE_HISTORY
+VALUES (
+    4,
+    1,
+    1,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    1
+);
+
+INSERT INTO SALE_HISTORY
+VALUES (
+    4,
+    1,
+    2,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    1
+);
+
+INSERT INTO SALE_HISTORY
+VALUES (
+    4,
+    1,
+    3,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    1
+);
+
+INSERT INTO SALE_HISTORY
+VALUES (
+    4,
+    1,
+    4,
+    TIMESTAMP '2022-1-15 8:00:00 -8:00',
+    1
+);
+
+--PART_TYPE
+INSERT INTO PART_TYPE
+VALUES (
+    1
+);
+
+INSERT INTO PART_TYPE
+VALUES (
+    2
+);
+
+--VENDOR
+INSERT INTO VENDOR
+VALUES (
+    1,
+    799,
+    'abc.com',
+    'ABC Microchips',
+    'C55R9',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+INSERT INTO VENDOR
+VALUES (
+    2,
+    800,
+    'cde.com',
+    'CDE Microchips',
+    'CDF5',
+    '100 Independence Dr',
+    'Menlo Park, CA 94025',
+    'Menlo Park',
+    'CA',
+    '94025'
+);
+
+--PART_TYPE_LISTING
+INSERT INTO PART_TYPE_LISTING
+VALUES (
+    1,
+    1,
+    200.0
+);
+
+INSERT INTO PART_TYPE_LISTING
+VALUES (
+    2,
+    1,
+    150.0
+);
+
+INSERT INTO PART_TYPE_LISTING
+VALUES (
+    1,
+    2,
+    1000.0
+);
+
+INSERT INTO PART_TYPE_LISTING
+VALUES (
+    2,
+    2,
+    1200.0
+);
+
+--PRODUCT_PART
+INSERT INTO PRODUCT_PART
+VALUES (
+    1,
+    1,
+    1,
+    4
+);
+
+INSERT INTO PRODUCT_PART
+VALUES (
+    1,
+    2,
+    2,
+    2
+);
+
+INSERT INTO PRODUCT_PART
+VALUES (
+    3,
+    1,
+    2,
+    2
+);
+
+INSERT INTO PRODUCT_PART
+VALUES (
+    3,
+    2,
+    1,
+    5
+);
+
+INSERT INTO PRODUCT_PART
+VALUES (
+    4,
+    1,
+    1,
+    1
+);
+
+INSERT INTO PRODUCT_PART
+VALUES (
+    4,
+    2,
+    2,
+    1
+);
+
+
